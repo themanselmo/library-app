a5ee0eae0b7be213958f2a65e09f724545580912
Stephen <stevriss22@gmail.com>
2021-10-25 17:38:32 -0400

    Added index.css file, Nav component, and some styling

--- library/src/components/App.js
+++ library/src/components/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import Header from  './Header'
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
     </div>
   );
 }
---
+++ library/src/components/Nav.js
@@ -0,0 +1,11 @@
+function Nav(){
+    return (
+        <div id="navBar">
+            <h1 id="search">search</h1>
+            <h4 id="donate">Donate</h4>
+            <h4 id="return">return</h4>
+        </div>
+    )
+}
+
+export default Nav
