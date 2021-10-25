f7c07b843279a254465ff983f70c57ef094e2d74
Stephen <stevriss22@gmail.com>
2021-10-25 17:07:49 -0400

    Added Header Component

---
+++ library/src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+function Header(){
+    return(
+        <div id="header">
+            <h1>BookStop</h1>
+            <img src="Users/stevenrisso/Desktop/Screen%20Shot%202021-10-25%20at%204.55.38%20PM.png"/>
+
+        </div>
+    );
+}
+
+export default Header;
