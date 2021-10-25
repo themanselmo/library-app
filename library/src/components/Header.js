96ef233c9ff25e42e0e3e4839835963e3661b99f
Stephen <stevriss22@gmail.com>
2021-10-25 17:18:35 -0400

    Added logo to page

--- library/src/components/Header.js
+++ library/src/components/Header.js
@@ -1,10 +1,10 @@
-
+import logo from '../images/BookStop2.png'
 
 function Header(){
     return(
         <div id="header">
-            <h1>BookStop</h1>
-            <img src="Users/stevenrisso/Desktop/Screen%20Shot%202021-10-25%20at%204.55.38%20PM.png"/>
+            
+            <img src= {logo}/>
 
         </div>
     );
