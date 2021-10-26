f327cc417035f0f7461f71440498be60fcd779be
max anselmo <itmanselmo@gmail.com>
2021-10-26 11:46:42 -0400

    added links to the nav to navigate between component routes

--- library/src/components/Nav.js
+++ library/src/components/Nav.js
@@ -1,9 +1,13 @@
+import {Link} from 'react-router-dom'
+
 function Nav(){
     return (
         <ul id="navBar">
-            <li id="search">search</li>
-            <li id="donate">Donate</li>
-            <li id="return">return</li>
+            <Link to={'/'}><li id="home">home</li> </Link>
+            <Link to={'/books/search'} ><li id="search">search</li> </Link>
+            <Link to={'/books/add'}><li id="donate">Donate</li></Link>
+            <Link to={''}><li id="return">return</li></Link>
+
         </ul>
     )
 }
