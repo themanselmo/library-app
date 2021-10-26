588442495a3621ac05e8107de4e2870f47c14861
max anselmo <itmanselmo@gmail.com>
2021-10-26 11:49:59 -0400

    capitalized nav options

--- library/src/components/Nav.js
+++ library/src/components/Nav.js
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom'
 function Nav(){
     return (
         <ul id="navBar">
-            <Link to={'/'}><li id="home">home</li> </Link>
-            <Link to={'/books/search'} ><li id="search">search</li> </Link>
+            <Link to={'/'}><li id="home">Home</li> </Link>
+            <Link to={'/books/search'} ><li id="search">Search</li> </Link>
             <Link to={'/books/add'}><li id="donate">Donate</li></Link>
-            <Link to={''}><li id="return">return</li></Link>
+            <Link to={''}><li id="return">Return</li></Link>
 
         </ul>
     )
