3c113cdda74a3bdc9ddfc8ffadb077d9bee7e8e7
max anselmo <itmanselmo@gmail.com>
2021-10-26 11:13:50 -0400

    added some books to display as recommended and implemented some basic routes to home, donate, and search

--- library/src/components/App.js
+++ library/src/components/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import {Route, Switch} from "react-router-dom"
+
 import Header from  './Header'
 import Nav from './Nav'
 import Overview from './Overview';
+import Search from './Search';
+import Donate from './Donate';
 
 function App() {
 
@@ -21,18 +25,61 @@ function App() {
     .then(data => setRentedBooks(data))
   }, [])
 
-  const mockBooks = [
+  const recommendedBooks = [
     {
-      title: "test title",
-      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ihVko-MgBxWTIaDy1jeo0AHaE7%26pid%3DApi&f=1",
-      author: "mr test"
-    }
+        "title": "Lord Of The Rings",
+        "author": "J.R.R Tolkien",
+        "image": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.collider.com%2Fwp-content%2Fuploads%2F2016%2F07%2Fthe-lord-of-the-rings-book-cover.jpg&f=1&nofb=1",
+        "genre": "fiction",
+        "checkedOut": false,
+        "id": 1
+      },
+      {
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "J.K. Rowling",
+        "image": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fd.ibtimes.co.uk%2Fen%2Ffull%2F1541432%2Fharry-potter-philosophers-stone.jpg&f=1&nofb=1",
+        "genre": "fiction",
+        "checkedOut": false,
+        "id": 2
+      },
+      {
+        "title": "Eloquent JavaScript",
+        "author": "Manjin Haverbeke",
+        "image": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-um47FqTbvPg%2FVh0a-qERKYI%2FAAAAAAAAD9M%2FAemef2DnUe0%2Fs1600%2FEloquent%2BJavaScript%2BFree%2BBook.png&f=1&nofb=1",
+        "genre": "textbook",
+        "checkedOut": false,
+        "id": 3
+      },
+      {
+        "title": "JavaScript Patterns",
+        "author": "Stoyan Stefanov",
+        "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F3.bp.blogspot.com%2F-ZB_AAD_1tiI%2FWKzhO9JKceI%2FAAAAAAAAH_c%2FElUSkJDNXDUDXEMPmpYuCWwy_jJBR0lUwCEw%2Fs1600%2Fjsp_javascript_patterns.jpg&f=1&nofb=1",
+        "genre": "textbook",
+        "checkedOut": false,
+        "id": 4
+      },
+      {
+        "title": "Embers Of War",
+        "author": "Gareth L. Powell",
+        "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2FgCX7EZXWnp097mMaNMhVMIsQFlw%3D%2F0x0%3A1181x1800%2F1200x0%2Ffilters%3Afocal(0x0%3A1181x1800)%3Ano_upscale()%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_asset%2Ffile%2F10131869%2FDEC171874.jpg&f=1&nofb=1",
+        "genre": "science fiction",
+        "checkedOut": false,
+        "id": 5
+      }
   ]
   return (
     <div className="App">
       <Header />
       <Nav />
-      <Overview recommendedBooks={mockBooks} rentedBooks={rentedBooks}/>
+      <Route path='/books/search'>
+        <Search />
+      </Route>
+      <Route path='/books/new'>
+        <Donate />
+      </Route> 
+      <Route exact path='/'>
+        <Overview recommendedBooks={recommendedBooks} rentedBooks={rentedBooks}/>
+      </Route>
     </div>
   );
 }
--- library/src/components/Overview.js
+++ library/src/components/Overview.js
@@ -4,8 +4,10 @@ const Overview = ({ rentedBooks, recommendedBooks}) => {
     return (
         <>
             {/* container for recommended books */}
+            <h1>Recommended Books:</h1>
             <BookContainer books={recommendedBooks}/>
             {/* container for rented books */}
+            <h1>Currently Rented Books:</h1>
             <BookContainer books={rentedBooks}/>
         </>
     )
