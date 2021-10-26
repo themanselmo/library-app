b19c482c7c489f22546b6b6065591d6316d87847
max anselmo <itmanselmo@gmail.com>
2021-10-26 16:38:15 -0400

    fixed nav link for donate

--- library/src/components/Nav.js
+++ library/src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav(){
         <ul id="navBar">
             <Link to={'/'}><li id="home">Home</li> </Link>
             <Link to={'/books/search'} ><li id="search">Search</li> </Link>
-            <Link to={'/books/add'}><li id="donate">Donate</li></Link>
+            <Link to={'/books/new'}><li id="donate">Donate</li></Link>
             <Link to={''}><li id="return">Return</li></Link>
         </ul>
     )
